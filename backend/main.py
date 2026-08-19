1c0ee6627f877fc789ed045ea42f4f8317cffb9e
Aaliyah2007 <abu2007.bilal@gmail.com>
2026-08-19 21:41:23 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from sqlalchemy import text
 
 from database import engine
-from .routes.auth_routes import router as auth_router
+from routes.auth_routes import router as auth_router
 from .routes.report_routes import router as report_router
 from .routes.image_routes import router as image_router
 
